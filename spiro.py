ad87ab6b355d53260e37eba033a3eb703b67f20b
ddeveloper72 <duncan.niamh@gmail.com>
2021-03-28 13:38:28 +0100

    set the paramaters for the class

--- spiro.py
+++ spiro.py
@@ -30,3 +30,26 @@ class Spiro:
 
         # initialize the drawing
         self.restart()
+
+
+# set the parameters
+def setparams(self, xc, yc, col, R, r, l):
+    # the Spirograph parameters
+    self.xc = xc
+    self.yc = yc
+    self.R = int(R)
+    self.r = int(r)
+    self.l = l
+
+    # reduce r/R to its smallest form by dividing with the GCD
+    gcdVal = gcd(self.r, self.R)
+    self.nRot = self.r//gcdVal
+
+    # get ratio of radii
+    self.k = r/float(R)
+
+    # set color
+    self.t.color(*col)
+
+    # store the current angle
+    self.a = 0
