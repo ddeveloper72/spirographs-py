a5d44dd01a2fa67b9f93bd9e383168889351e5f9
ddeveloper72 <duncan.niamh@gmail.com>
2021-03-28 14:33:04 +0100

    adding random parameter generator

--- spiro.py
+++ spiro.py
@@ -144,3 +144,18 @@ class SpiroAnimator:
 
         # call timer
         turtle.ontimer(self.update, self.deltaT)
+
+
+# generate random parameters
+def genRandomParams(self):
+    width, height = self.width, self.height
+    R = random.randint(50, min(width, height)//2)
+    r = random.randint(10, 9*R//10)
+    l = random.uniform(0.1, 0.9)
+    xc = random.randint(-width//2, width//2)
+    yc = random.randint(-height//2, height//2)
+    col = (random.random(),
+           random.random(),
+           random.random()
+           )
+    return(xc, yc, col, R, r, l)
